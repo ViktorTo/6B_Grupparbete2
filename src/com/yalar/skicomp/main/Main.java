cf4e52a7d2f2f124a7dd16501c83a371155c4f8f
Simon Ulander <devsimonulander@gmail.com>
2021-11-25 10:24:56 +0100

    Testar config

--- src/com/yalar/skicomp/main/Main.java
+++ src/com/yalar/skicomp/main/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hejsan");
 		System.out.println("Svejsan");
-
+		// *Testar Git*
+		System.out.println("Git fungerar nu.");
 	}
 
 }
