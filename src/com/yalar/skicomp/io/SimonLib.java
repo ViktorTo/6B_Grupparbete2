be33df897a4373ec62e3628a1dbd4ea24961d390
Simon Ulander <devsimonulander@gmail.com>
2021-12-07 10:35:46 +0100

    Changed the StringInput() to accept all non-empty strings.

--- src/com/yalar/skicomp/io/SimonLib.java
+++ src/com/yalar/skicomp/io/SimonLib.java
@@ -49,34 +49,22 @@ public class SimonLib {
 
 	public static String stringInput() {
 
-		char[] list = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-				't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö' };
-
 		String s = "";
-		String s_test = "";
 
-		int num = -1;
-		while (num == -1) {
+		boolean success = false;
+		while (!success) {
 			System.out.print("> ");
 			try {
 				s = scan.nextLine();
-				s_test = s.toLowerCase();
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
-			try {
-				for (int i = 0; i < list.length; i++) {
-					for (int j = 0; j < s_test.length(); j++) {
-						if (s.charAt(j) == list[i]) {
-							num = 1;
-						}
-					}
-				}
-			} catch (Exception e) {
-				System.out.println("Only enter strings with letters in them!");
-				num = -1;
+			if (s.isEmpty()) {
+				System.out.println("You didn't enter anything!");
+				success = false;
+			} else {
+				success = true;
 			}
 		}
 
